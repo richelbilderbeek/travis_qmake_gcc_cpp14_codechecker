d6930786de5e7b714227794750296f60e2d74a1b
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-06 12:53:40 +0100

    Break build

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include <cassert>
 #include <iostream>
 
-//C++14
-auto f() noexcept {
-  return "Hello world\n";
-}
-
 int main() 
 {
   #ifdef NDEBUG
@@ -13,7 +8,5 @@ int main()
   #endif
   int a[3] = { 0, 1, 2 };
   a[4] = 0; // Access violation!
-  std::cout
-    << a[4] //Access violation!
-    << f() << '\n';
+  std::cout << a[5] << '\n'; //Access violation!
 }
